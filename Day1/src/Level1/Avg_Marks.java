48399b3d895350bdecd6c7cc92b20ad6b669bb4c
Khushi Goyal <khushigoyal9314@gmail.com>
2025-06-12 12:24:11 +0530

    Add day1 folder with level1

---
+++ Day1/src/Level1/Avg_Marks.java
@@ -0,0 +1,16 @@
+package Level1;
+
+public class Avg_Marks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Maths=94;
+		int Physics=95;
+		int Chemistry=96;
+		int avg=(Maths+Physics+Chemistry)/3;
+		System.out.print("Sam's average mark in PCM is: "+ avg);
+		
+
+	}
+
+}
