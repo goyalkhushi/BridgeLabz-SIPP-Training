a733606358373f75218ed12258cafef560f0b0ab
Khushi Goyal <khushigoyal9314@gmail.com>
2025-06-13 11:11:02 +0530

    Control flow ques1

---
+++ ControlFlow/Level1/level1/Divisible_by_five.java
@@ -0,0 +1,13 @@
+package level1;
+import java.util.*;
+public class Divisible_by_five {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+	        int number = sc.nextInt();
+	        System.out.println("Is the number " + number + " divisible by 5? " + (number % 5 == 0));
+	   
+
+	}
+
+}
---
+++ ControlFlow/Level1/level1/First_three_smallest_num.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class First_three_smallest_num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
