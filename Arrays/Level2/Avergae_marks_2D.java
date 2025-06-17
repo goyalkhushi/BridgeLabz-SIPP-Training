285f32f800798de67c9d9b1e2c247405298adc14
Khushi Goyal <khushigoyal9314@gmail.com>
2025-06-17 11:42:19 +0530

    [Khushi Goyal], Added 2question of  Level2 Assignment  Of Arrays

--- Arrays/Level2/Avergae_marks_2D.java
+++ Arrays/Level2/Avergae_marks_2D.java
@@ -1,10 +1,46 @@
 package level2;
+import java.util.*;
 
 public class Avergae_marks_2D {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
 
+        int[][] marks = new int[n][3]; // [physics, chemistry, maths]
+        double[] percentage = new double[n];
+        char[] grade = new char[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter marks for Student " + (i + 1));
+
+            for (int j = 0; j < 3; j++) {
+                String subject = (j == 0) ? "Physics" : (j == 1) ? "Chemistry" : "Maths";
+                System.out.print(subject + ": ");
+                marks[i][j] = sc.nextInt();
+                if (marks[i][j] < 0) {
+                    System.out.println("Invalid! Re-enter for the same student.");
+                    j--;
+                }
+            }
+
+            percentage[i] = (marks[i][0] + marks[i][1] + marks[i][2]) / 3.0;
+
+            if (percentage[i] >= 90) grade[i] = 'A';
+            else if (percentage[i] >= 75) grade[i] = 'B';
+            else if (percentage[i] >= 60) grade[i] = 'C';
+            else if (percentage[i] >= 40) grade[i] = 'D';
+            else grade[i] = 'F';
+        }
+
+        System.out.println("\nPhysics\tChemistry\tMaths\tPercentage\tGrade");
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%d\t%d\t\t%d\t%.2f%%\t\t%c\n",
+                marks[i][0], marks[i][1], marks[i][2], percentage[i], grade[i]);
+        }
+		
 	}
 
 }
--- Arrays/Level2/Digit_Finder.java
+++ Arrays/Level2/Digit_Finder.java
@@ -1,9 +1,35 @@
 package level2;
+import java.util.*;
 
 public class Digit_Finder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+        
+        int number = sc.nextInt();
+
+        int[] digits = new int[20];
+        int[] frequency = new int[10]; // for digits 0 to 9
+        int index = 0;
+
+        
+        while (number != 0) {
+            digits[index] = number % 10;
+            number /= 10;
+            index++;
+        }
+        
+        for (int i = 0; i < index; i++) {
+            frequency[digits[i]]++;
+        }
+
+        System.out.println("Digit Frequencies:");
+        for (int i = 0; i < 10; i++) {
+            if (frequency[i] > 0) {
+                System.out.println("Digit " + i + ": " + frequency[i] + " times");
+            }
+        }
 
 	}
 
