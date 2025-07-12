552cf2ec4e25432cb3b88039a3c7c358d5c56464
Khushi Goyal <khushigoyal9314@gmail.com>
2025-07-12 16:44:44 +0530

    [khuhsi] done ques1 of linear and binary search using StringBuilder

---
+++ LinearAndBinarySearch/Reverse.java
@@ -0,0 +1,14 @@
+package linearandbinarysearch;
+import java.util.*;
+
+public class Reverse {
+	public static void main(String[] args) {
+		StringBuilder sb=new StringBuilder();
+		String st="hello";
+		sb.append(st);
+		
+		System.out.print(sb.reverse());
+		
+	}
+
+}
